d758dd935baad92f3d9a8b57d02c2af19323d2f8
Rushikesh Sudhakar Karangale <162353734+rushikarangale@users.noreply.github.com>
2024-08-10 17:01:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//image 1 code 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+//image 2 code
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//whos player wins code
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! 😀";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins! 😀";
+    document.querySelector("h1").style.color=("#cc4eb9");
+
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+  
+  //button
+  const refreshBtn = document.querySelector("button");
+
+function handleClick() {
+  window.location.reload();
+}
+
+refreshBtn.addEventListener("click", handleClick);
